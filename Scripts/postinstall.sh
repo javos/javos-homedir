2643087c80af406eccfc6b16c9593b605e4366ac
Janis von Seggern <hallo@janisvonseggern.de>
2025-07-11 13:51:24 +0200

    Specified user install flathub

--- Scripts/postinstall.sh
+++ Scripts/postinstall.sh
@@ -24,39 +24,39 @@ fi
 
 echo "Installing Flatpak Apps"
 
-flatpak install flathub com.cassidyjames.butler -y
-flatpak install flathub com.daidouji.oneko -y
-flatpak install flathub com.github.k4zmu2a.spacecadetpinball -y
-flatpak install flathub com.github.tchx84.Flatseal -y
-flatpak install flathub com.github.wwmm.easyeffects -y
-flatpak install flathub com.google.Chrome -y
-flatpak install flathub com.heroicgameslauncher.hgl -y
-flatpak install flathub com.obsproject.Studio -y
-flatpak install flathub com.rtosta.zapzap -y
-flatpak install flathub com.spotify.Client -y
-flatpak install flathub com.stremio.stremio -y
-flatpak install flathub com.todoist.Todoist -y
-flatpak install flathub com.usebottles.bottles -y
-flatpak install flathub com.valvesoftware.Steam -y
-flatpak install flathub com.visualstudio.code -y
-flatpak install flathub de.haeckerfelix.Shortwave -y
-flatpak install flathub dev.qwery.AddWater -y
-flatpak install flathub fm.reaper.Reaper -y
-flatpak install flathub io.github.dvlv.boxbuddyrs -y
-flatpak install flathub io.github.flattool.Ignition -y
-flatpak install flathub io.github.flattool.Warehouse -y
-flatpak install flathub io.github.Foldex.AdwSteamGtk -y 
-flatpak install flathub io.github.jeffshee.Hidamari -y 
-flatpak install flathub io.github.peazip.Peazip.Addon.i386 -y
-flatpak install flathub net.nokyan.Resources -y
-flatpak install flathub org.blender.Blender -y
-flatpak install flathub org.gnome.Epiphany -y
-flatpak install flathub org.gnome.meld -y
-flatpak install flathub org.gnome.World.PikaBackup -y
-flatpak install flathub org.libreoffice.LibreOffice -y
-flatpak install flathub org.mozilla.Thunderbird -y
-flatpak install flathub org.rncbc.qpwgraph -y
-flatpak install flathub org.signal.Signal -y
-flatpak install flathub org.telegram.desktop -y
-flatpak install flathub org.videolan.VLC -y
-flatpak install flathub org.virt_manager.virt-manager -y
+flatpak install --user flathub com.cassidyjames.butler -y
+flatpak install --user flathub com.daidouji.oneko -y
+flatpak install --user flathub com.github.k4zmu2a.spacecadetpinball -y
+flatpak install --user flathub com.github.tchx84.Flatseal -y
+flatpak install --user flathub com.github.wwmm.easyeffects -y
+flatpak install --user flathub com.google.Chrome -y
+flatpak install --user flathub com.heroicgameslauncher.hgl -y
+flatpak install --user flathub com.obsproject.Studio -y
+flatpak install --user flathub com.rtosta.zapzap -y
+flatpak install --user flathub com.spotify.Client -y
+flatpak install --user flathub com.stremio.stremio -y
+flatpak install --user flathub com.todoist.Todoist -y
+flatpak install --user flathub com.usebottles.bottles -y
+flatpak install --user flathub com.valvesoftware.Steam -y
+flatpak install --user flathub com.visualstudio.code -y
+flatpak install --user flathub de.haeckerfelix.Shortwave -y
+flatpak install --user flathub dev.qwery.AddWater -y
+flatpak install --user flathub fm.reaper.Reaper -y
+flatpak install --user flathub io.github.dvlv.boxbuddyrs -y
+flatpak install --user flathub io.github.flattool.Ignition -y
+flatpak install --user flathub io.github.flattool.Warehouse -y
+flatpak install --user flathub io.github.Foldex.AdwSteamGtk -y 
+flatpak install --user flathub io.github.jeffshee.Hidamari -y 
+flatpak install --user flathub io.github.peazip.Peazip.Addon.i386 -y
+flatpak install --user flathub net.nokyan.Resources -y
+flatpak install --user flathub org.blender.Blender -y
+flatpak install --user flathub org.gnome.Epiphany -y
+flatpak install --user flathub org.gnome.meld -y
+flatpak install --user flathub org.gnome.World.PikaBackup -y
+flatpak install --user flathub org.libreoffice.LibreOffice -y
+flatpak install --user flathub org.mozilla.Thunderbird -y
+flatpak install --user flathub org.rncbc.qpwgraph -y
+flatpak install --user flathub org.signal.Signal -y
+flatpak install --user flathub org.telegram.desktop -y
+flatpak install --user flathub org.videolan.VLC -y
+flatpak install --user flathub org.virt_manager.virt-manager -y
